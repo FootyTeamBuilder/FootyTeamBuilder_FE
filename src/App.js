c0be782b6d17e4b2b7966499997c36426f784fed
nhachoc1999 <nhachoc1999@gmail.com>
2022-06-12 20:48:26 +0700

    fix: fix_conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import Footer from "./components/footer/Footer.jsx";
-import Header from './components/header/Header.jsx';
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
 
 function App() {
   return (
-      <div className="App"> 
     <BrowserRouter>
-    <Header />
-      <Routes>
+      <div className="App">
+        <Routes>
           <Route path="/" element={<Dashboard />} />
-        </Routes>
-        <Routes>
-          <Route exact path='/' />
-          <Route path="*" element={<Footer />} />
-        </Routes>
-        <Routes>
-          <Route path="/login" element={ <Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-    </BrowserRouter>
       </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,71 +1,120 @@
-import Button from '../../components/button/Button';
-import HoverImage from '../../components/homepage/hover-image/HoverImage';
-import './Dashboard.css';
-import Footer from '../../components/footer/Footer';
+import Button from "../../components/button/Button";
+import HoverImage from "../../components/homepage/hover-image/HoverImage";
+import "./Dashboard.css";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
 
 const Dashboard = () => {
-    return (
-        <div className="dashboard">
-            <div className="story story-1st">
-                <div className="left">
-                    <h1 style={{fontSize: '3rem'}}>Tạo đội bóng và tìm kiếm đối thủ của bạn</h1>
-                    <p style={{fontSize: '1.1rem'}}>
-                        Quản lý đội bóng của bạn dễ dàng. 
-                        Tìm kiếm đối thủ phù hợp với đội bóng của bạn.
-                    </p>
-                    <Button text='Đăng ký ngay' height='3rem' width='10rem' fontSize='1rem' />
-                </div>
-                <div className="right">
-                    {/* <img src={require('../../assets/messi.jpg')} alt='' /> */}
-                </div>
-            </div>
-            <div className="story story-2nd">
-                <img src={require('../../assets/thay-park.jpg')} alt='' style={{width: '671px', height: '450px'}} />
-                <div className="right">
-                    <h1 style={{fontSize: '2.5rem', marginBottom: '5rem'}}>Nhanh chóng và tiện lợi</h1>
-                    <div className="two-child-div">
-                        <div className="child">
-                            <img src={require('../../assets/tactics.png')} style={{width: '5rem'}} alt='' />
-                            <h3>Tổ chức đội bóng</h3>
-                            <p>Tạo đội bóng, thêm thành viên, quản lý thông tin đội bóng, nhắn tin, tổ chức chiến thuật.</p>
-                        </div>
-                        <div className="child">
-                            <img src={require('../../assets/handshake.png')} style={{width: '5rem'}} alt='' />
-                            <h3>Tìm kiếm đối thủ</h3>
-                            <p>Tìm kiếm đối thủ phù hợp về trình độ dựa trên bảng phân hạng đội bóng.</p>
-                        </div>
-                    </div>
-                    <Button text='Đăng ký ngay' height='3rem' width='10rem' fontSize='1rem' />
-                </div>
-            </div>
-            <div className="story story-3rd">
-                <div className="title">
-                    <h1 style={{fontSize: '2.5rem', margin: 0}}>Đa dạng các chức năng</h1>
-                    <p style={{textAlign: 'center'}}>Cung cấp mọi công cụ để bạn tổ chức một trận đấu.</p>
-                </div>
-                <div className="grid">
-                    <HoverImage image='pep.jpg' text='Quản lý thông tin đội bóng' />
-                    <HoverImage image='battay.jpg' text='Bắt đối phù hợp' />
-                    <HoverImage image='strategy.jpg' text='Điều chỉnh chiến thuật' />
-                    <HoverImage image='feedback.jpg' text='Trao đổi tin nhắn trực tuyến' />
-                    <HoverImage image='statistics.jpg' text='Lịch sử đấu và thống kê' />
-                    <HoverImage image='ranking.jpg' text='Xếp hạng đội bóng' />
-                </div>
-            </div>
-            <div className="story story-4th">
-                <h1 style={{fontSize: '2.5rem'}}>Đánh giá của người dùng</h1>
-                <img src={require('../../assets/reviews.png')} style={{height: '11rem'}} alt="" />
-            </div>
-            <div className="story story-5th">
-                <h1 style={{fontSize: '2rem', marginBottom: '2.5rem'}}>Subscribe để nhận thông báo mới nhất</h1>
-                <div className="row">
-                    <input type="text" placeholder='Nhập email của bạn' />
-                    <Button text='Subscribe' height='3rem' width='7rem' />
-                </div>
-            </div>
-            <Footer />
+  return (
+    <>
+      <Header />
+      <div className="dashboard">
+        <div className="story story-1st">
+          <div className="left">
+            <h1 style={{ fontSize: "3rem" }}>
+              Tạo đội bóng và tìm kiếm đối thủ của bạn
+            </h1>
+            <p style={{ fontSize: "1.1rem" }}>
+              Quản lý đội bóng của bạn dễ dàng. Tìm kiếm đối thủ phù hợp với đội
+              bóng của bạn.
+            </p>
+            <Button
+              text="Đăng ký ngay"
+              height="3rem"
+              width="10rem"
+              fontSize="1rem"
+            />
+          </div>
+          <div className="right">
+            {/* <img src={require('../../assets/messi.jpg')} alt='' /> */}
+          </div>
         </div>
-    )
-}
+        <div className="story story-2nd">
+          <img
+            src={require("../../assets/thay-park.jpg")}
+            alt=""
+            style={{ width: "671px", height: "450px" }}
+          />
+          <div className="right">
+            <h1 style={{ fontSize: "2.5rem", marginBottom: "5rem" }}>
+              Nhanh chóng và tiện lợi
+            </h1>
+            <div className="two-child-div">
+              <div className="child">
+                <img
+                  src={require("../../assets/tactics.png")}
+                  style={{ width: "5rem" }}
+                  alt=""
+                />
+                <h3>Tổ chức đội bóng</h3>
+                <p>
+                  Tạo đội bóng, thêm thành viên, quản lý thông tin đội bóng,
+                  nhắn tin, tổ chức chiến thuật.
+                </p>
+              </div>
+              <div className="child">
+                <img
+                  src={require("../../assets/handshake.png")}
+                  style={{ width: "5rem" }}
+                  alt=""
+                />
+                <h3>Tìm kiếm đối thủ</h3>
+                <p>
+                  Tìm kiếm đối thủ phù hợp về trình độ dựa trên bảng phân hạng
+                  đội bóng.
+                </p>
+              </div>
+            </div>
+            <Button
+              text="Đăng ký ngay"
+              height="3rem"
+              width="10rem"
+              fontSize="1rem"
+            />
+          </div>
+        </div>
+        <div className="story story-3rd">
+          <div className="title">
+            <h1 style={{ fontSize: "2.5rem", margin: 0 }}>
+              Đa dạng các chức năng
+            </h1>
+            <p style={{ textAlign: "center" }}>
+              Cung cấp mọi công cụ để bạn tổ chức một trận đấu.
+            </p>
+          </div>
+          <div className="grid">
+            <HoverImage image="pep.jpg" text="Quản lý thông tin đội bóng" />
+            <HoverImage image="battay.jpg" text="Bắt đối phù hợp" />
+            <HoverImage image="strategy.jpg" text="Điều chỉnh chiến thuật" />
+            <HoverImage
+              image="feedback.jpg"
+              text="Trao đổi tin nhắn trực tuyến"
+            />
+            <HoverImage image="statistics.jpg" text="Lịch sử đấu và thống kê" />
+            <HoverImage image="ranking.jpg" text="Xếp hạng đội bóng" />
+          </div>
+        </div>
+        <div className="story story-4th">
+          <h1 style={{ fontSize: "2.5rem" }}>Đánh giá của người dùng</h1>
+          <img
+            src={require("../../assets/reviews.png")}
+            style={{ height: "11rem" }}
+            alt=""
+          />
+        </div>
+        <div className="story story-5th">
+          <h1 style={{ fontSize: "2rem", marginBottom: "2.5rem" }}>
+            Subscribe để nhận thông báo mới nhất
+          </h1>
+          <div className="row">
+            <input type="text" placeholder="Nhập email của bạn" />
+            <Button text="Subscribe" height="3rem" width="7rem" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

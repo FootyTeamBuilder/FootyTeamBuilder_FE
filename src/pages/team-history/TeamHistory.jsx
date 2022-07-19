359d29b65cbc971453560c164ddfa84f0fa3083a
tranducanh <tdanh041100@gmail.com>
2022-07-19 16:45:22 +0700

    lich su dau va update tran

--- src/pages/team-history/TeamHistory.jsx
+++ src/pages/team-history/TeamHistory.jsx
@@ -1,80 +1,72 @@
-import './TeamHistory.css';
-import { useNavigate } from 'react-router-dom';
+import "./TeamHistory.css";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import moment from "moment";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import Spinner from "../../components/loading/Spinner";
+import Timeline from "@mui/lab/Timeline";
+import TimelineComponent from "../../components/update-match/timeline-item/TimelineComponent";
+import HistoryItem from "../../components/team-history/history-item/HistoryItem";
 
 const TeamHistory = () => {
-
     const navigate = useNavigate();
+    const { teamId } = useParams();
+    const [matchHistoryList, setMatchHistoryList] = useState();
+    const [isCaptain, setIsCaptain] = useState();
+    const [openHelp, setOpenHelp] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const user = useSelector((state) => state.auth.login?.currentUser);
+
+    const getMatchHistory = async () => {
+        const response = await axios.get(`/match/match-history/${teamId}`, {
+            headers: { Authorization: `Bearer ${user?.token}` },
+        });
+        setMatchHistoryList(response.data.data);
+        setIsLoading(false);
+    };
+
+    //kiem tra user la doi truong cua team dang xem hay khong
+    const checkUser = async (teamId) => {
+        const response = await axios.get(`/team/is-captain/${teamId}`, {
+            headers: { Authorization: `Bearer ${user?.token}` },
+        });
+        setIsCaptain(response.data.isCaptain);
+    };
+
+    useEffect(() => {
+        getMatchHistory();
+        checkUser(teamId);
+    }, []);
+
+    if (isLoading) return <Spinner />;
 
     return (
         <div className="team-history">
-            <div className="matches"> {/* sua thanh map */}
-                <div className="match-item">
-                    <div className="left" style={{backgroundColor: '#5dd469'}}>
-                        Thắng
-                    </div>
-                    <div className="middle">
-                        <div className="score-middle">
-                            <p className='team'>FC 1</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='score'>2</p>
-                            <p>-</p>
-                            <p className='score'>1</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='team'>FC 2</p>
-                        </div>
-                        <div className="match-info">
-                            <div className="time">17:00</div>
-                            <div className="area">Sân thông tin</div>
-                        </div>
-                    </div>
-                    <button 
-                        className='update-btn'
-                        onClick={(e) => navigate('/update-match')}
-                    >Cập nhật</button>
-                </div>
-                <div className="match-item">
-                    <div className="left" style={{backgroundColor: '#e34f4f'}}>
-                        Thua
-                    </div>
-                    <div className="middle">
-                        <div className="score-middle">
-                            <p className='team'>FC 1</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='score'>1</p>
-                            <p>-</p>
-                            <p className='score'>2</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='team'>FC 2</p>
-                        </div>
-                        <div className="match-info">
-                            <div className="time">17:00</div>
-                            <div className="area">Sân thông tin</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="match-item">
-                    <div className="left" style={{backgroundColor: 'gray'}}>
-                        Hoà
-                    </div>
-                    <div className="middle">
-                        <div className="score-middle">
-                            <p className='team'>FC 1</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='score'>1</p>
-                            <p>-</p>
-                            <p className='score'>1</p>
-                            <img src={require('../../assets/blank-avatar.jpg')} className='avatar' alt="" />
-                            <p className='team'>FC 2</p>
-                        </div>
-                        <div className="match-info">
-                            <div className="time">17:00</div>
-                            <div className="area">Sân thông tin</div>
-                        </div>
-                    </div>
-                </div>
+            <div className="matches">
+                {matchHistoryList?.map((m) => {
+                    return (
+                        <HistoryItem key={m._id} match={m} isCaptain={isCaptain} />
+                    );
+                })}
             </div>
+            <div className="help-btn" onClick={(e) => setOpenHelp(!openHelp)}>
+                <ContactSupportIcon />
+            </div>
+            {openHelp && (
+                <p className="help-div">
+                    Chú thích các trạng thái trận đấu: <br />
+                    - Conflict: Tỉ số trận đấu cập nhật bởi 2 đội đang mâu
+                    thuẫn. <br />- Confirm: Tỉ số trận đấu cập nhật bởi 2 đội đã
+                    trùng nhau. <br />
+                    - Pending: Tỉ số trận đấu đang được cập nhật. <br />
+                    - None: Chưa cập nhật tỉ số. <br />
+                </p>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default TeamHistory
+export default TeamHistory;
